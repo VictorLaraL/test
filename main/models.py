76d67f1c03aa4c11a22ed2faeedb93efde5eb7b2
Victor Manuel Lara Lopez <vicblack15@hotmail.com>
2022-03-18 21:12:03 -0600

    feature: create custom endpoints

--- main/models.py
+++ main/models.py
@@ -1,9 +1,8 @@
 from sqlite3 import Timestamp
 from django.db import models
-from paranoid_model.models import Paranoid
 
 
-class Device(Paranoid):
+class Device(models.Model):
     name = models.CharField(max_length=150)
     type_device = models.ForeignKey("TypeDevice", on_delete=models.CASCADE)
     start_date = models.DateTimeField()
@@ -15,21 +14,21 @@ class Device(Paranoid):
         ordering = ["name"]
 
 
-class TypeDevice(Paranoid):
+class TypeDevice(models.Model):
     type_device = models.CharField(max_length=150)
 
     class Meta:
         ordering = ["type_device"]
 
 
-class StatusDevice(Paranoid):
+class StatusDevice(models.Model):
     status_device = models.CharField(max_length=150)
 
     class Meta:
         ordering = ["status_device"]
 
 
-class Reading(Paranoid):
+class Reading(models.Model):
     device = models.ForeignKey("Device", on_delete=models.CASCADE)
     type_device = models.ForeignKey("TypeDevice", on_delete=models.CASCADE)
     actual_power = models.DecimalField(max_digits=19, decimal_places=10)
@@ -39,7 +38,7 @@ class Reading(Paranoid):
         ordering = ["device"]
 
 
-class Binnacle(Paranoid):
+class Binnacle(models.Model):
     device = models.ForeignKey("Device", on_delete=models.CASCADE)
     actual_power = models.DecimalField(max_digits=19, decimal_places=10)
     date = models.DateTimeField()
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from urllib import response
+from django.http import JsonResponse
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets, generics
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -8,6 +10,8 @@ from .serializers import (
     TypeDeviceSerializer,
     BinnacleSerializer,
 )
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class DeviceViewSet(viewsets.ModelViewSet):
@@ -17,6 +21,22 @@ class DeviceViewSet(viewsets.ModelViewSet):
     search_fields = ["name"]
     pagination_class = PageNumberPagination
 
+    @action(detail=True, methods=["get"])
+    def get_devices_by_type(self, request, pk=None, *args, **kwargs):
+        instance = Device.objects.filter(type_device=pk)
+        
+        list_instances = []
+
+        for record in instance:
+            list_instances.append({
+                "id": record.id,
+                "name": record.name,
+                "type_device": record.type_device.type_device,
+                "actual_power":record.actual_power,
+                "status_device": record.status_device.status_device,
+                })
+
+        return Response(list_instances)
 
 class ListStatusDeviceViewSet(generics.ListAPIView):
     queryset = StatusDevice.objects.all()
@@ -38,3 +58,32 @@ class BinnacleViewSet(viewsets.ModelViewSet):
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ["device__name", "date"]
     pagination_class = PageNumberPagination
+
+    @action(detail=True, methods=["get"])
+    def get_records_by_device(self, request, pk=None, *args, **kwargs):
+        instance = Binnacle.objects.filter(device=pk)
+
+        list_binnacle = []
+
+        for record in instance:
+            list_binnacle.appen({
+                "id":record.id,
+                "device": record.device.name,
+                "actual_power": record.actual_power,
+                "date":record.date,
+            })
+
+        return Response(list_binnacle)
+    
+    @action(detail=True, methods=["get"])
+    def get_power_by_device(self, request, pk=None, *args, **kwargs):
+        instance = Binnacle.objects.filter(device=pk)
+        energy = 0
+
+        for record in instance:
+            energy += float(record.actual_power)
+
+        return Response({
+            "device": instance.device.name,
+            "total_energy": energy,
+        })
